450724a9d86401b48bfee65b5a9e6da782db38fb
Maziarz <maziarz.maciej@gmail.com>
2016-11-22 19:36:32 +0100

    ITakeDamage, Player class

--- Dramaturgis/Player.cs
+++ Dramaturgis/Player.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Dramaturgis
 {
-    class Player
+    class Player : ITakeDamage
     {
+        public int healthPoints { get; set; }             
+        public int manaPoints { get; set; }           
+        public int attackValue { get; set; }         
+        public Bags bag { get; set; }           
+        public Equip equipment { get; set; }        
+        
+
+        void dealDamageToEnemy()                       
+        {
+
+        }
+        void ITakeDamage.takeDamageFromEnemy()          
+        {
+
+        }
+
+        
+        
+
+
     }
 }

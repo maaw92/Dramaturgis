43a36b2ff379cad9b3875c6c07c6538d1fe0d6db
Maziarz <maziarz.maciej@gmail.com>
2016-11-22 19:47:37 +0100

    Monster class, player name> player class

--- Dramaturgis/Monster.cs
+++ Dramaturgis/Monster.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dramaturgis
+namespace Dramaturgis 
 {
-    class Monster
+    abstract class Monster : ITakeDamage
     {
+        public string monsterName { get; set; }
+        public int healthPoints { get; set; }
+        public int manaPoints { get; set; }
+        public int attackDamageValue { get; set; }
+        public int defenseValue { get; set; }
+
+        void attack()
+        {
+
+        }
+        void ITakeDamage.takeDamageFromEnemy()
+        {
+
+        }
     }
 }
--- Dramaturgis/Player.cs
+++ Dramaturgis/Player.cs
@@ -8,9 +8,11 @@ namespace Dramaturgis
 {
     class Player : ITakeDamage
     {
+        public string playerName { get; set; }
         public int healthPoints { get; set; }             
         public int manaPoints { get; set; }           
-        public int attackValue { get; set; }         
+        public int attackValue { get; set; }
+        public int defenseValue { get; set; }
         public Bags bag { get; set; }           
         public Equip equipment { get; set; }        
         
@@ -22,6 +24,15 @@ namespace Dramaturgis
         void ITakeDamage.takeDamageFromEnemy()          
         {
 
+        }
+        Player (string playerName)
+        {
+            healthPoints = 100;
+            manaPoints = 100;
+            attackValue = 10;
+            defenseValue = 10;
+            Bags bag = new Bags();
+            Equip equipment = new Equip();
         }
 
         
